1d00e1dc3792a6ba67fa368fc985df9c6dee47f2
Muhammad Aqeel <aqeeltm1998@gmail.com>
2023-03-11 22:10:28 +0530

    Update feature_extraction.py

--- preprocessing_tool/feature_extraction.py
+++ preprocessing_tool/feature_extraction.py
@@ -3,7 +3,7 @@
 
 # +
 import sys
-sys.path.append("E:\FYP\Server 2\FYP-All-files\preprocessing_tool")
+sys.path.append("https://github.com/AqeelMuhammad/FYP-All-files/tree/main/preprocessing_tool")
 
 from noise_reduction import *
 from peak_detection import *
